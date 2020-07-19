110de15cc99d41c9b1978cd909274999a49b415f
Q1139821 <david_leon_gonzalez@carrefour.com>
2020-07-19 19:27:18 +0200

    Json list added

---
+++ src/test/java/com/example/arraylist/testinglist/ListTests.java
@@ -0,0 +1,27 @@
+package com.example.arraylist.testinglist;
+
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Optional;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ListTests {
+
+
+	@Test
+	public void getValuesNotExistFromArrayList() {
+
+		final String name = "David";
+		final Optional<String> opt = Optional.ofNullable(name);
+		assertEquals("David", opt.get());
+	}
+
+}
