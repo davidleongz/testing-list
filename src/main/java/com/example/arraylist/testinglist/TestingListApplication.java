0e4f541c0de4836b71a526839f0e7a95685e12cf
Q1139821 <david_leon_gonzalez@carrefour.com>
2020-07-19 18:03:26 +0200

    Initial commit

---
+++ src/main/java/com/example/arraylist/testinglist/TestingListApplication.java
@@ -0,0 +1,13 @@
+package com.example.arraylist.testinglist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/arraylist/testinglist/TestingListApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.arraylist.testinglist;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class TestingListApplicationTests {
+
+	@Test
+	void contextLoads() {}
+
+}
